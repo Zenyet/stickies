f366e0e48ad64c495c25010129c0331655119c79
Zenyet <52519506+Zenyet@users.noreply.github.com>
2025-05-28 10:55:39 +0800

    fix

--- components/note-editor-dialog.tsx
+++ components/note-editor-dialog.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Save, Trash2 } from "lucide-react";
-import type { Note } from "@/store";
+import type { Note } from "@/types";
 import {
   Dialog,
   DialogContent,
